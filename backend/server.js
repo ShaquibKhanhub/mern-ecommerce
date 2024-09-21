2a70aa5b88be93a30741398182844d2a7f22ed89
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-09-22 01:34:47 +0530

    complete code

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,34 @@
-// const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/productRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //allows us to accept JSON data in the req.body
+const __dirname = path.resolve(); // Required for using absolute path for serving files
 
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+// API Routes
 app.use("/api/products", productRoute);
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+// Start the server
 app.listen(PORT, () => {
-  connectDB();
-  console.log(`server is running on port 5000 http://localhost:${PORT} `);
+  connectDB(); // Connect to the database
+  console.log(`Server started at http://localhost:${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box minH={"100vh"} bg={useColorModeValue('gray.100','gray.900')}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/create" exact element={<CreatePage />} />
+          </Routes>
+        </Router>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
